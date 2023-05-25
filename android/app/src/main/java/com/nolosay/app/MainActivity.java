78b1af3255577bdde0dd7d26785d7e79cdb72835
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-05-25 12:01:43 +0200

    First commit

---
+++ android/app/src/main/java/com/nolosay/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.nolosay.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-yellow-700">
+      <span>HEY</span>
+    </main>
+  )
+}
