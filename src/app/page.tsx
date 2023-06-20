f89820d45230d7077282ac4ce5a954720d195003
Johan Chrillesen <johan.chrillesen@epitech.eu>
2023-06-20 20:36:44 +0200

    Add camera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,49 @@
-import Image from 'next/image'
+"use client";
+import React, { useRef, useState, useEffect } from 'react';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-yellow-700">
-      <span>HEY</span>
-    </main>
-  )
+const CamScreen = () => {
+
+    const videoRef = useRef<any>(null);
+    const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+
+    const sendVideoData = (data: string) => {
+        socket.emit("videoData", data);
+    };
+
+    useEffect(() => {
+        const enableVideoStream = async () => {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                setMediaStream(stream);
+            } catch (error) {
+                console.error('Error accessing webcam', error);
+            }
+        };
+
+        enableVideoStream();
+    }, []);
+
+    useEffect(() => {
+        if (videoRef.current && mediaStream) {
+            videoRef.current.srcObject = mediaStream;
+        }
+    }, [videoRef, mediaStream]);
+
+    useEffect(() => {
+        return () => {
+            if (mediaStream) {
+                mediaStream.getTracks().forEach((track) => {
+                    track.stop();
+                });
+            }
+        };
+    }, [mediaStream]);
+
+    return (
+        <div>
+            <video ref={videoRef} autoPlay={true} />
+        </div>
+    )
+
 }
+export default CamScreen;
